f34bf6bcfa8084317bf775a7608b25344396b763
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-02-12 21:09:55 -0200

    Added a health bar

---
+++ TheFlyingHouseWizard/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour 
+{
+	public float maxHealth;
+	private float currentSize;
+	private float maxSize;
+
+	private void Start() 
+	{
+		maxSize = transform.localScale.x;
+		currentSize = maxSize;
+	}
+
+	public void SetHealth(float health)
+	{
+		health = Mathf.Clamp(health, 0, maxHealth);
+
+		currentSize = (maxSize * health) / maxHealth;
+
+		StopCoroutine("ResizeBarCoroutine");
+		StartCoroutine("ResizeBarCoroutine");
+	}
+
+	public IEnumerator ResizeBarCoroutine()
+	{
+		float duration = 0.3f;
+		float timer = 0;
+
+		Vector3 scale = transform.localScale;
+		float startScaleX = scale.x;
+
+		while(timer < 1)
+		{
+			yield return new WaitForEndOfFrame();
+			
+			timer += Time.deltaTime / duration;
+			Debug.Log(timer);
+
+			scale.x = Mathf.Lerp(startScaleX, currentSize, timer);
+			transform.localScale = scale;
+		}
+	}
+}
--- TheFlyingHouseWizard/Assets/Scripts/Player/Player.cs
+++ TheFlyingHouseWizard/Assets/Scripts/Player/Player.cs
@@ -4,26 +4,37 @@ public class Player : MonoBehaviour
 {
 	public PlayerStats stats;
 
+	public HealthBar healthBar;
+
 	private CharacterHealth health;
 
 	private void Awake() 
 	{
 		health = GetComponent<CharacterHealth>();
 		health.SetMaxHealth(stats.maxHealth, true);
+
+		healthBar.maxHealth = stats.maxHealth;
 	}
 
 	private void OnEnable() 
 	{
 		health.OnDie += OnDie;
+		health.OnHealthChange += OnHealthChange;
 	}
 
 	private void OnDisable() 
 	{
 		health.OnDie -= OnDie;
+		health.OnHealthChange -= OnHealthChange;
 	}
 
 	private void OnDie()
 	{
 		GameManager.Instance.RestartScene();
+	}
+
+	private void OnHealthChange(float currentHealth)
+	{
+		healthBar.SetHealth(currentHealth);
 	}
 }
