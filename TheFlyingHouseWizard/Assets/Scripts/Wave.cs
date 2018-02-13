90da21c2f6b3c95c3c5774188d0e04e430f7c6ec
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-02-13 10:09:21 -0200

    Created a wave system
    - Created Wave Script to control the wave behavior;
    - Created WaveManager Script to receive the waves events;

---
+++ TheFlyingHouseWizard/Assets/Scripts/Wave.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour 
+{
+	[System.Serializable]
+	public struct DelayedThreat
+	{
+		public float delay;
+		public GameObject threat;
+	}
+
+	public float startDelay;
+	public DelayedThreat[] delayedThreats;
+	public Wave nextWave;
+	public bool finalWave;
+
+	private Transform threats;
+
+	private void Awake() 
+	{
+		threats = transform.Find("Threats");
+		threats.gameObject.SetActive(false);
+	}
+	private void Start() 
+	{
+		Invoke("StartWave", startDelay);
+	}
+
+	private void StartWave()
+	{
+		SendMessageUpwards("WaveStarted", null, SendMessageOptions.DontRequireReceiver);
+
+		threats.gameObject.SetActive(true);
+
+		InvokeRepeating("CheckWaveEnded", 0, 1);
+	}
+
+	private void CheckWaveEnded()
+	{
+		int childCount = threats.childCount;
+
+		if(childCount <= 0)
+		{
+			SendMessageUpwards("WaveEnded", finalWave, SendMessageOptions.DontRequireReceiver);
+
+			if(!finalWave)
+				StartNextWave();
+		}
+	}
+
+	private void StartNextWave()
+	{
+		nextWave.gameObject.SetActive(true);
+
+		Destroy(this.gameObject);
+	}
+}
---
+++ TheFlyingHouseWizard/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour 
+{
+	private void WaveStarted()
+	{
+		Debug.Log("Wave Started");
+	}
+
+	private void WaveEnded(bool isFinal)
+	{
+		Debug.Log("Wave Ended");
+
+		if(isFinal)
+			GameManager.Instance.RestartScene();
+	}
+}
